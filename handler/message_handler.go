a33f862f13f8b6ff01d59d5b6ba1a6c7874d8d3d
babiswas2020 <bapan1680814@gmail.com>
2026-01-18 17:00:57 +0530

    updated handler

--- handler/message_handler.go
+++ handler/message_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"app/helper"
+	"app/logger"
 	"context"
 	"net/http"
 	"time"
@@ -10,23 +11,28 @@ import (
 )
 
 func MessageHandler(ctx *gin.Context) {
+	logger := logger.LoggingInit()
 	c, cancel := context.WithTimeout(ctx.Request.Context(), 10*time.Second)
 	defer cancel()
 
+	logger.Info("validating http request.")
 	result, err := helper.ValidateRequest(c, ctx.Request.Header)
 	if err != nil {
 		if err == context.DeadlineExceeded {
+			logger.Error("Request deadline exceeded.")
 			ctx.JSON(http.StatusRequestTimeout, gin.H{
 				"error": "request timed out",
 			})
 			return
 		}
 
+		logger.Error("Internal error occured.")
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"error": err.Error(),
 		})
 		return
 	}
+	logger.Info("Sucessfully validated the request.")
 	data := map[string]string{"user_agent": result}
 	ctx.AsciiJSON(http.StatusOK, data)
 }
